921bb55d1402a7d8f3fd13b8c14100138be227ea
mabdullahsoyturk <msoyturk20@ku.edu.tr>
2022-05-20 16:34:47 +0300

    fix transpose test

--- tests/test_transpose.py
+++ tests/test_transpose.py
@@ -2,24 +2,26 @@ import torch
 import torch_sputnik
 from utils.util import *
 
-def transpose(m, k, n, nnz):
-    a = torch.arange(1, nnz + 1, dtype=torch.float32).view(m, k)
-    values, row_indices, row_offsets, column_indices = dense_to_sparse(a)
+def sparse_transpose(sparse, m, k, n):
+    values, row_indices, row_offsets, column_indices = dense_to_sparse(sparse)
+    output_values = values.clone()
+    output_row_offsets = row_offsets.clone()
+    output_column_indices = column_indices.clone()
 
-    b = torch.arange(1, nnz + 1, dtype=torch.float32).view(m, k)
-    out_values, out_row_indices, out_row_offsets, out_column_indices = dense_to_sparse(b)
+    torch_sputnik.csr_transpose(m, n, values, row_offsets, column_indices, output_values, output_row_offsets, output_column_indices)
 
-    print('Before transpose:')
-    print(values.size())
-    print(values)
-
-    nonzeros = torch.IntTensor([nnz])
-    
-    torch_sputnik.csr_transpose(m, n, nonzeros, values, row_offsets, column_indices, out_values, out_row_offsets, out_column_indices)
-    
-    print('\nAfter transpose:')
-    print(out_values.size())
-    print(out_values)
+    return output_values
 
 if __name__ == "__main__":
-    transpose(8, 8, 8, 64)
+    m, k, n = 64, 64, 64
+    sparse = torch.rand((m * k), dtype=torch.float32).view(m, k).cuda()
+
+    sparse_result = sparse_transpose(sparse, m, k, n).reshape(k, m)
+
+    print(sparse_result)
+    print(sparse.t())
+
+    if ((sparse_result - sparse.t()) < 1e4).sum().item() == m * n:
+        print("Results match")
+    else:
+        print("Results don't match")
